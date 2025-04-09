ba141af49caabfea69fb99d842b1a9d90faaf870
Dennis Liu <genius_dennis1224@hotmail.com>
2025-04-09 13:02:11 -0400

    Initial Commit: newly created ASP.NET Model and View Project

---
+++ gang_quiz4_dotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gang_quiz4_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
